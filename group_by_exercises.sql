52764e0dac937f045d04f5ffb3896f2b76325ec1
kicknback <joshua.borreli@gmail.com>
2021-07-29 14:11:53 -0500

    Finished group by exercise file

---
+++ group_by_exercises.sql
@@ -0,0 +1,34 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e'
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT last_name, first_name FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e'
+GROUP BY last_name, first_name
+ORDER BY last_name;
+
+SELECT last_name FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT COUNT(last_name), last_name FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT COUNT(*), gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
