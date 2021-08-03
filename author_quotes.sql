0032f1d26628874c117fcb664ec1569740a582c3
kicknback <joshua.borreli@gmail.com>
2021-08-03 12:16:17 -0500

    More additions to authors/quotes

--- author_quotes.sql
+++ author_quotes.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS quotables_db;
+
 CREATE DATABASE if not exists quotables_db;
 
 use quotables_db;
@@ -21,6 +23,24 @@ VALUES ('Douglas', 'Adams'),
 SELECT *
 FROM authors;
 
+DROP TABLE IF EXISTS topics;
+
+CREATE TABLE topics
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO topics(name)
+VALUES ('Space and Time'),
+       ('Humor'),
+       ('Office Life'),
+       ('Hitchhiker''s Guide to the Galaxy');
+
+SELECT *
+FROM topics;
+
 
 DROP TABLE IF EXISTS quotes;
 
@@ -28,9 +48,44 @@ CREATE TABLE quotes
 (
     id        INT UNSIGNED NOT NULL AUTO_INCREMENT,
     author_id INT UNSIGNED NOT NULL,
-    content   TEXT NOT NULL,
+    content   TEXT         NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (author_id) references authors (id)
 );
 
+#Seed quotes
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
+        'I love deadlines. I love the whooshing noise they make as they go by.');
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
+        'Time is an illusion. Lunchtime doubly so.');
+INSERT INTO quotes (author_id, content)
+values ((select id from authors where first_name = 'Mark' and last_name = 'Twain'),
+        'Clothes make the man. Naked people have little or no influence on society.');
+INSERT INTO quotes (author_id, content)
+values ((select id from authors where first_name = 'Kurt' and last_name = 'Vonnegut'),
+        'The universe is a big place, perhaps the biggest.');
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'), 'Don''t Panic.');
+
 DESCRIBE quotes;
+
+SELECT *
+FROM quotes;
+
+DROP TABLE IF EXISTS quote_topic;
+
+CREATE TABLE quote_topic
+(
+    quote_id INTEGER UNSIGNED NOT NULL,
+    topic_id INTEGER UNSIGNED NOT NULL,
+    FOREIGN KEY (quote_id) REFERENCES quotes (id),
+    FOREIGN KEY (topic_id) REFERENCES topics (id)
+);
+
+INSERT INTO quote_topic(quote_id, topic_id)
+VALUES (1, 2), (1, 3), (2, 4), (3, 1),
+       (3, 2), (3, 3), (3, 4), (4, 2), (5, 1);
+
+SELECT * FROM quote_topic;
